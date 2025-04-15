5e118660cb88afa4651200a0541c13e80a90f5c5
Suvam093 <suvamshaw86@gmail.com>
2025-04-15 18:51:35 +0530

    few tweaks in backend index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,35 +6,35 @@ import cookieParser from 'cookie-parser';
 import messageRoutes from './routes/message.route.js';
 import cors from 'cors';
 import { server, app} from './lib/socket.js'; 
+import path from "path";
 
-import path from 'path'; 
 
 dotenv.config();
 
-app.use(cors({                        //enable CORS for all routes basically cross url resource sharing from backend to frontend
-    origin: 'http://localhost:5173',  // frontend url
-    credentials: true,                // allow credentials (cookies) to be sent
-}))
-
-app.use(express.json({limit:'10mb'}))
-app.use(cookieParser()) 
-
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
-const __dirname = path.resolve(); // get the current directory
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-app.use("/api/auth", authRoutes)
-app.use("/api/messages", messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '/frontend/dist'))); // serve static files from the frontend build folder
-    
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html")); // send the index.html file for all other routes
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
-    console.log("Server is running on port:"+ PORT);
-    connectDB()
-})  
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
