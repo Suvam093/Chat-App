efe7a2425e0c2ca526060787b5988035650e1f16
Suvam093 <suvamshaw86@gmail.com>
2025-04-15 18:36:15 +0530

    fixing the route specific problem

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -6,9 +6,11 @@ import { getMessages, sendMessage } from '../controllers/message.controller.js';
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar)
-router.get("/:id", protectRoute, getMessages) // get the user by id
 
+//this is a more specific route using /send/:id thats why it comes before just /:id route 
 router.post("/send/:id", protectRoute, sendMessage) // send message
 
+// if we use this before then it will steal all the routes with /:id parameter
+router.get("/:id", protectRoute, getMessages) // get the user by id
 
 export default router;
